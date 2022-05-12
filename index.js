9247120bc3f6e28a316062d86bbba7e299ba75cf
CarlEverest78 <thepythoneer@gmail.com>
2022-05-12 16:48:14 +0200

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,112 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+// player variables
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+let sumEl = document.getElementById("player-sum")
+let cardsEl = document.getElementById("player-cards")
+
+// computer variables
+let computerCards = []
+let computerSum = 0
+let computerHasBlackJack = false
+let computerIsAlive = false
+
+let computerSumEl = document.getElementById("computer-sum")
+let computerCardsEl = document.getElementById("computer-cards")
+let passAmounts = [17,18,19,20]
+
+// message variable
+let messageEl = document.getElementById("message-el")
+
+
+function getRandomCard() {
+    let randomNumber = (Math.floor(Math.random() * 12) + 1)
+
+    if (randomNumber === 1) {
+        return 11
+    } else if (randomNumber > 10) {
+        return 10
+    } else {
+        return randomNumber
+    }
 }
 
-console.log(message)
+function startGame() {
+    document.getElementById('btn-new').style.display = "inline";
+    cards = []
+    sum = 0
+    computerCards = []
+    computerSum = 0
+    computerCardsEl.textContent = "Computer kaarten: "
+    computerSumEl.textContent = "Computer totaal: "
+    hasBlackJack = false
+    isAlive = true
+    cards.push(getRandomCard())
+    cards.push(getRandomCard())
+    sum = cards[0] + cards[1]
+    document.getElementById('btn-start').style.display = "none";
+    document.getElementById('btn-pass').style.display = "inline";
+    runGame()
+}
+
+function runGame() {
+    cardsEl.textContent = "Uw kaarten:" 
+    for(var i=0; i < cards.length; i++) 
+        {cardsEl.textContent += " " + cards[i]}
+    sumEl.textContent = "Uw totaal: " + sum
+    if (sum <= 20) {
+        message = "Wil je nog een kaart trekken?"
+    } else if (sum === 21) {
+        message = "Hoera, je hebt 21!!!"
+        hasBlackJack = true
+        document.getElementById('btn-start').style.display = "inline";
+        document.getElementById('btn-pass').style.display = "none";
+        document.getElementById('btn-new').style.display = "none";
+    } else {
+        message = "Helaas, meer dan 21."
+        isAlive = false
+        document.getElementById('btn-start').style.display = "inline";
+        document.getElementById('btn-pass').style.display = "none";
+        document.getElementById('btn-new').style.display = "none";
+    }
+    
+    messageEl.textContent = message
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        cards.push(card)
+        sum += card
+        runGame()
+    }
+}
+
+
+function pass() {
+    document.getElementById('btn-start').style.display = "inline";
+    document.getElementById('btn-pass').style.display = "none";
+    document.getElementById('btn-new').style.display = "none";
+
+    let passAmount = passAmounts[Math.floor(Math.random() * passAmounts.length)]
+    while (computerSum < passAmount) {
+        let card = getRandomCard()
+        computerCards.push(card)
+        computerSum += card
+    }
+    computerCardsEl.textContent = "Computer kaarten:" 
+    for(var i=0; i < computerCards.length; i++) 
+        {computerCardsEl.textContent += " " + computerCards[i]}
+        computerSumEl.textContent = "Computer totaal: " + computerSum
+
+    if (computerSum === 21 && sum === 21) {
+        messageEl.textContent = "Helaas, geen winnaar."
+    } else if (computerSum < 22 && computerSum > sum) {
+        messageEl.textContent = "Helaas, de computer wint."
+    } else {
+        messageEl.textContent = "Gefeliciteerd je hebt gewonnen!"
+    }
+}
